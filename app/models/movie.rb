27c919798aa888c4f4e280c12b7a2ea4fd8b7870
Vi Tu <vttu@ucsc.edu>
2018-08-26 21:01:57 -0700

    Showing/Leaving reviews for each movie worked

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ApplicationRecord
     has_and_belongs_to_many :genres
-    has_many :reviews
+    has_many :reviews, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :reviews
+  resources :genres
+  resources :movies do
+    resources :reviews
+  end
+
+
   get 'static_pages/home'
   get 'static_pages/about'
-  resources :genres
-  resources :movies
-
   root 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
